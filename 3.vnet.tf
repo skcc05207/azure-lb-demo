e806ee3f325e853e6695b3e31d4950abe3733d96
Soodong, Kim <37996396+skcc05207@users.noreply.github.com>
2021-09-14 09:55:47 +0900

    Update 3.vnet.tf

--- 3.vnet.tf
+++ 3.vnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_virtual_network" "david-net" {
-    name = "david-vnet"
-    address_space = ["40.0.0.0/16"]
-    location = azurerm_resource_group.david-rg.location
-    resource_group_name = azurerm_resource_group.david-rg.name
+resource "azurerm_virtual_network" "user01-net" {
+    name = "user01-vnet"
+    address_space = ["1.0.0.0/16"]
+    location = azurerm_resource_group.user01-rg.location
+    resource_group_name = azurerm_resource_group.user01-rg.name
 }
