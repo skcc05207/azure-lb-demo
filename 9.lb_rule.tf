6eff82f42af657450b5ba7972835a823b3e75602
Soodong, Kim <37996396+skcc05207@users.noreply.github.com>
2021-09-14 10:00:58 +0900

    Update 9.lb_rule.tf

--- 9.lb_rule.tf
+++ 9.lb_rule.tf
@@ -1,11 +1,11 @@
 resource "azurerm_lb_rule" "lbrule" {
-    resource_group_name = azurerm_resource_group.david-rg.name
-    loadbalancer_id = azurerm_lb.david-lb.id
+    resource_group_name = azurerm_resource_group.user01-rg.name
+    loadbalancer_id = azurerm_lb.user01-lb.id
     name = "http"
     protocol = "Tcp"
     frontend_port = 80
     backend_port = 80
-    backend_address_pool_id = azurerm_lb_backend_address_pool.david-bpepool.id
-    frontend_ip_configuration_name = "davidPublicIPAddress"
-    probe_id = azurerm_lb_probe.david-lb-probe.id
+    backend_address_pool_id = azurerm_lb_backend_address_pool.user01-bpepool.id
+    frontend_ip_configuration_name = "user01PublicIPAddress"
+    probe_id = azurerm_lb_probe.user01-lb-probe.id
 }
