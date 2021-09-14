565758a2fd1a05bea3536618bc25a67ad6a6e3d3
Soodong, Kim <37996396+skcc05207@users.noreply.github.com>
2021-09-14 09:59:43 +0900

    Update 8.lb_probe.tf

--- 8.lb_probe.tf
+++ 8.lb_probe.tf
@@ -1,6 +1,6 @@
-resource "azurerm_lb_probe" "david-lb-probe" {
-    resource_group_name = azurerm_resource_group.david-rg.name
-    loadbalancer_id = azurerm_lb.david-lb.id
+resource "azurerm_lb_probe" "user01-lb-probe" {
+    resource_group_name = azurerm_resource_group.user01-rg.name
+    loadbalancer_id = azurerm_lb.user01-lb.id
     name = "http-probe"
     protocol = "Http"
     request_path = "/"
