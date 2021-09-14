9420814a3220b761c3db934d1b67abca7ae9b2aa
Soodong, Kim <37996396+skcc05207@users.noreply.github.com>
2021-09-14 09:50:25 +0900

    Update 16.myDiagStorage.tf

--- 16.myDiagStorage.tf
+++ 16.myDiagStorage.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "mystorageaccount" {
-    name                = "davidvm1diagestorage1"
-    resource_group_name = azurerm_resource_group.david-rg.name
-    location = azurerm_resource_group.david-rg.location
+    name                = "user01vm1diagestorage1"
+    resource_group_name = azurerm_resource_group.user01-rg.name
+    location = azurerm_resource_group.user01-rg.location
     account_replication_type = "LRS"
     account_tier = "Standard"
 
